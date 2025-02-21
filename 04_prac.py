43e357cbbe45421e757eb0153bf56ab8648f196d
Ahnaf Abir <ahnafhossainabir222@gmail.com>
2025-02-20 22:36:52 -0600

    prac4

---
+++ 04_prac.py
@@ -0,0 +1,12 @@
+#Print the elements of the following list using a loop:
+# [1,4,9,16,25,36,49,64,81,100]
+l=[1,4,9,16,25,36,49,64,81,100]
+for n in l:
+   print(n)
+
+#While loop:
+h=[1,4,9,16,25,36,49,64,81,100]
+i=0
+while i < len(h):
+   print(h[i])
+   i+=1
