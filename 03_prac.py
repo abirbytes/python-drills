90ad6118cf346dcc1810a71372589c57a469d000
Ahnaf Abir <ahnafhossainabir222@gmail.com>
2025-02-20 22:36:46 -0600

    prac3

---
+++ 03_prac.py
@@ -0,0 +1,11 @@
+#Print the multiplication table of a number n
+n =6
+for i in range(0,11):
+   print(f"{n} x {i} = {n*i}")
+
+#While loop
+s = int(input("Enter the num:"))
+l=0
+while l<=10:
+   print(f"{s} x {l} = {s*l}")
+   l+=1
