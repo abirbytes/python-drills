49cee56501d3299faa38ba924876c7d16cd1b8c5
Ahnaf Abir <ahnafhossainabir222@gmail.com>
2025-02-20 22:37:07 -0600

    prac 6

---
+++ 06_prac_imp.py
@@ -0,0 +1,18 @@
+#WAP to find the sum of first n numbers (using while loop)
+n=5
+i=0
+sum=0
+while i<=n:
+   sum+=i
+   i+=1
+print(sum)
+
+#For loop
+x=int(input("Enter the num:"))
+sum=0
+for i in range(x+1):
+   sum+=i
+   i+=1
+print(sum)
+
+
