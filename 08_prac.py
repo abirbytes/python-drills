177c2918414ef9cbe09e1a05c9e3e2453e138718
Ahnaf Abir <ahnafhossainabir222@gmail.com>
2025-02-21 20:54:18 -0600

    prac

---
+++ 08_prac.py
@@ -0,0 +1,10 @@
+def calc_sum(a,b):
+   sum=a+b
+   print(sum)#Prints the sum but gives none when says to print the function as the value is not stored in it.
+   # return a+b #it stores the value in the function
+a=calc_sum(2,3)
+print(a)
+
+def print_hello():
+   print('Hello')
+print_hello()
